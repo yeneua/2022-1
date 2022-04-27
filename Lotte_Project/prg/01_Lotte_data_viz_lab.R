053bf2ab90fbe2a1ea39df99b863338c7882a4ff
yeneua <76564438+yeneua@users.noreply.github.com>
2022-04-27 17:41:42 +0900

    9-1 대면수업

--- Lotte_Project/prg/01_Lotte_data_viz_lab.R
+++ Lotte_Project/prg/01_Lotte_data_viz_lab.R
@@ -76,4 +76,54 @@ boxplot(s1$cnt~s1$점포ID)
 
 
 load(file="data/s1.rda")
+load(file="data/tb.rda")
 head(s1)
+head(tb)
+
+#10월 데이터 없앨것
+s1 <- subset(s1,거래월 != 10) #거래월이 10월이 아닌 것
+boxplot(s1$amount ~s1$거래월)
+
+# 0. ggplot2 라이브러리 불러오기
+library(ggplot2)
+
+# 1. x,y축 무슨 데이터 쓸건지. 데이터 지정
+g <- ggplot(s1, #데이터 : s1
+            aes(점포ID, amount, fill = 점포ID)) #x:점포ID, y:amount. fill(채우기)
+
+#ggplot : layer 단위, +로 계속 추가
+g <- g+geom_boxplot()+
+  scale_fill_brewer(palette = "Set3")+
+  stat_summary(fun.y = "mean", geom = "point", shape = 23, size = 3, fill = "red")+  #=>평균이 중앙값보다 높다. CC - outlier때문에 평균 높다 
+  stat_boxplot(geom = "errorbar")+
+  labs(title = "롯데 매장 매출액 Boxplot",
+       subtitle = "매출액 vs 점포명",
+       x = "점포명", y = "매출액",
+       caption = "Source : 롯데데이터, red = 평균")
+g
+# => CC outlier가 조금 있다. AA가 매출 제일 많다.
+
+# s1 데이터 확인
+# 상품 대분류별로 그림그리기
+(g1 <- ggplot(s1, aes(상품대분류명, amount))+
+  geom_point(aes(col = 점포ID, size = cnt))+
+  labs(title = "Bubble Chart", subtitle = "점포별 : 품목 vs 매출액",
+       x = "품목", y = "매출"))
+
+#동그라미가 왜 여러개 ?? -> 왜 그럴까 ? 
+# => 월별로 데이터가 다 들어가서 그럼
+
+s2 <- tb %>%  #s1은 월별, 점포와 다 섞인 데이터
+  group_by(상품대분류명, 점포ID) %>% 
+  summarise(amount = sum(round(구매금액/1000,0)), cnt = sum(구매건수))
+
+(g1 <- ggplot(s2, aes(상품대분류명, amount))+
+    geom_point(aes(col = 점포ID, size = cnt))+ #size:구매건수
+    labs(title = "Bubble Chart", subtitle = "점포별 : 품목 vs 매출액",
+         x = "품목", y = "매출"))
+# => 축산; AA와 CC가 거래건수는 비슷한데 거래금액이 차이가 많이 난다. AA가 매출이 가장 높다.
+# => 낙농; AA의 매출, 거래건수가 높다.
+
+#다양한 속성을 가지고 그림을 그림. 시각 속성을 활용해 많은 정보 표시
+
+
