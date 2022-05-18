3c9df4e2ca1b378c2c13d83bcd13421f8e7c9471
yeneua <76564438+yeneua@users.noreply.github.com>
2022-05-19 04:33:49 +0900

    12-1 대면수업

--- data_viz/Map_Project/prg/prg01.R
+++ data_viz/Map_Project/prg/prg01.R
@@ -27,6 +27,10 @@ ggplot(korea[korea$id==1,], aes(x=long,y=lat, group=group, color=id)) +
   geom_polygon(fill="white") +
   theme(legend.position="none")
 
+ggplot(korea[korea$id==0,], aes(x=long,y=lat, group=group, color=id)) + 
+  geom_polygon(fill="white") +
+  theme(legend.position="none") #0:서울,1:부산,2:대구 ,3:인천, 4:광주, 5:대전, 6:울산, 7: 세종시, 8:경기도,9:강원도,10:충청북도,11:충남,12:전북,13:전남,14:경북,15:경남,16:제주
+
 ##제주도
 (ggplot(korea[korea$id==16,], aes(x=long,y=lat, group=group, color=id)) + 
   geom_polygon(fill="white") +
@@ -53,3 +57,92 @@ library(RColorBrewer)
 ggplot(korea, aes(x=long, y=lat, group=group, fill=diff)) +
   geom_polygon() +
   scale_fill_gradientn(colors = brewer.pal(10, name = "YlGnBu"))
+
+
+
+
+##미주
+#graphics.off()
+sidoshp@proj4string
+#spTransform() 함수를 이용하여 좌표계 변환
+korea <- (spTransform(sidoshp, CRS("+proj=longlat"))) 
+korea@proj4string
+pop <- read.csv("data/popData.csv")
+pop <- subset(pop, id!=7) #세종시 제거
+korea <- fortify(korea)
+korea <- subset(korea, id!=7)
+
+korea_map <- merge(korea, pop, by="id")
+library(RColorBrewer)
+ggplot(korea_map, aes(long, lat, group=group, fill=diff)) +
+  geom_polygon()+
+  scale_fill_gradientn(colors = brewer.pal(9, name= "YlGnBu"))
+
+
+
+#12-1(5/18)
+busan <- read.csv("./data/busan_sigungu.csv")
+busan #사회안전지수(4개의 영역)
+#사회안전시수가 높을수록 살기 좋은 지역
+#지표를 구하기에 구가 너무 넓다 - 통 단위로 연구 했었음 ..
+#안전지수 무엇하고 관련이 있을까? grdp?? 고민해보기 ..
+#무엇이 가장 영향?
+
+cor(busan[,4:8]) #상관계수
+#index - economic과(경제활동) 관련이 가장 높다
+#기장군, 강서구-점점 좋아짐
+
+
+rm(list=ls()) #모든 데이터셋을 삭제
+install.packages("GISTools")
+#install.packages("rgeos")
+#install.packages("maptools")
+install.packages("rgdal")
+library(GISTools)
+#library(maptools)
+library(ggplot2)
+library(dplyr)
+library(rgdal)
+library(RColorBrewer)
+
+sigungushp = readOGR("sigungu/sig.shp")
+summary(sigungushp)
+df_map = fortify(sigungushp) # R 데이터 프레임으로 변경
+head(df_map,20)
+df_map_info = sigungushp@data
+head(df_map_info,50)
+df_map_info[, "id"] = (1:nrow(df_map_info)) - 1 #0~249.  id로 merge
+head(df_map_info,50) #생성된 id컬럼 확인
+
+df_map_info[, "SIDO"] = as.numeric(substr(df_map_info$SIG_CD,
+                                          start = 1, stop = 2)) #SIDO컬럼 생성
+head(df_map_info,50) #생성된 SIDO컬럼 - substr로 앞에 숫자 2개만 추출했음(서울인지, 부산인지 ,, 부산은 26)
+
+df_map_info_busan = df_map_info[df_map_info$SIDO==26,]  #부산만추출. 서울은 11, 대구는27
+df_map_info_busan
+
+df_map_busan = df_map[df_map$id %in% df_map_info_busan$id, ] #df_map(전체맵)에서 아이디가 같은 것만 가지고  오기
+head(df_map_busan,20)
+
+busan <- read.csv("./data/busan_sigungu.csv")
+busan 
+
+# 2. map과 사회안전지수 데이터를 병합
+busan_map <- merge(busan, df_map_busan,by="id")
+head(busan_map, 20)
+
+(ggplot(busan_map, aes(long,lat,group=group, fill=index))+
+  geom_polygon()+
+  scale_fill_gradientn(colors = brewer.pal(9, name = "YlGnBu")))
+
+
+#ggplot으로 map chart를 그림
+(ggplot(busan_map, aes(long,lat,group=group, fill=economic))+ #fill=economic
+    geom_polygon()+
+    geom_text(busan_map, aes(long,lat,group=group,label=economic))+
+    scale_fill_gradientn(colors = brewer.pal(9, name = "YlGnBu")))
+
+
+
+
+## 시험 -> 지금 공부하는 내용. 작년 시험문제 줄 것 - 유형파악.실습.오픈북
